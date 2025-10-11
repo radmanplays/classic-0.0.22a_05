54270381cb249d5f7d62eab6c7cc5a81ed91db65
radmanplays <95340057+radmanplays@users.noreply.github.com>
2025-10-11 19:29:00 +0330

    fix resizing

--- src/game/java/com/mojang/minecraft/Minecraft.java
+++ src/game/java/com/mojang/minecraft/Minecraft.java
@@ -330,6 +330,21 @@ public final class Minecraft implements Runnable {
 								Level var5 = var44.minecraft.level;
 								LevelRenderer var6 = var44.minecraft.levelRenderer;
 								ParticleEngine var49 = var44.minecraft.particleEngine;
+								if (Display.wasResized()) {
+									if(Display.getHeight() != 0) {
+										this.width = Display.getWidth();
+										this.height = Display.getHeight();
+										if(this.hud !=null) {
+											this.hud = new InGameHud(this, this.width, this.height);
+										}
+										
+										if(this.screen != null) {
+											Screen sc = this.screen;
+											this.setScreen((Screen)null);
+											this.setScreen(sc);
+										}
+									}
+								}
 								GL11.glViewport(0, 0, var44.minecraft.width, var44.minecraft.height);
 								Level var54 = var44.minecraft.level;
 								Player var60 = var44.minecraft.player;
@@ -488,21 +503,6 @@ public final class Minecraft implements Runnable {
 
 								var44.minecraft.hud.render();
 							} else {
-								if (Display.wasResized()) {
-									if(Display.getHeight() != 0) {
-										this.width = Display.getWidth();
-										this.height = Display.getHeight();
-										if(this.hud !=null) {
-											this.hud = new InGameHud(this, this.width, this.height);
-										}
-										
-										if(this.screen != null) {
-											Screen sc = this.screen;
-											this.setScreen((Screen)null);
-											this.setScreen(sc);
-										}
-									}
-								}
 								GL11.glViewport(0, 0, var44.minecraft.width, var44.minecraft.height);
 								GL11.glClearColor(0.0F, 0.0F, 0.0F, 0.0F);
 								GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT | GL11.GL_COLOR_BUFFER_BIT);
